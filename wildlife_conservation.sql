68e6bfd9bbb5e945fedaa0508e674d6c1d666e9c
Shovon96 <fakhruddinshovon@gmail.com>
2025-05-25 01:40:57 +0600

    Inserted in the table successfully

--- wildlife_conservation.sql
+++ wildlife_conservation.sql
@@ -25,6 +25,28 @@ CREATE Table sightings (
     note TEXT
 );
 
+-- Insert the data in Rengers table
+INSERT INTO rangers (rangers_id, rangers_name, region) VALUES
+(1, 'Alice Green', 'Northern Hills'),
+(2, 'Bob White', 'River Delta'),
+(3, 'Carol King', 'Mountain Range');
+
+-- Insert the data in Species table
+INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+-- Insert the data in Sighting table
+INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, note) VALUES
+(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+
+-- Execute all the tables
 SELECT * FROM rangers;
 SELECT * FROM species;
 SELECT * FROM sightings;
